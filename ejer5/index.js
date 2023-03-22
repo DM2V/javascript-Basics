7053398d0664d888c0aaa189c3c3a27845f30f26
DM2V <marcelo.malte11@gmail.com>
2023-03-22 16:25:58 -0500

    Add exercise

---
+++ ejer5/index.js
@@ -0,0 +1,17 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+let hight = 165;
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let hightM = 1.65;
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let weight = 60.5;
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+let hightMUp = Math.ceil(hightM);
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let weightDown = Math.floor(weight);
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let isEquals = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
