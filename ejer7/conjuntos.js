361e5b994cef5a08d7fd9001359304704cb427ed
DM2V <marcelo.malte11@gmail.com>
2023-04-10 20:51:55 -0500

    set_exercise

---
+++ ejer7/conjuntos.js
@@ -0,0 +1,12 @@
+/*
+Crea 
+- Un nuevo Set con los nombres de tu familia
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)  
+
+*/
+
+const family = new Set(["Marcelo", "Kevin", "Fanny"]);
+
+family.add("Marcelo");
+family.add("Javascript");
